aca66adbfe5c36aa30c4d8aaf708f2e39bd5194d
Mathieu Monin <mathieu.monin@digitalnolimit.com>
2020-10-26 00:06:37 +0100

    Try fix ci

--- Scripts/carthage.sh
+++ Scripts/carthage.sh
@@ -15,5 +15,8 @@ trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
 echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200 = arm64 arm64e armv7 armv7s armv6 armv8' >> $xcconfig
 echo 'EXCLUDED_ARCHS = $(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT)__XCODE_$(XCODE_VERSION_MAJOR))' >> $xcconfig
 
+echo 'ONLY_ACTIVE_ARCH=NO' >> $xcconfig
+echo 'VALID_ARCHS = $(inherited) x86_64' >> $xcconfig
+
 export XCODE_XCCONFIG_FILE="$xcconfig"
 carthage "$@"
